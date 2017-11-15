c072190c0cbe32052617613af4d02bcb9cea1217
Hai Tran <hai.tran@2clicksolutions.com>
2017-11-15 17:01:04 +0700

    Make the app works

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  itemCount : number = 4;
+  buttonText : string = "Add an item";
+  goalText = "My first life goal";
+  goals = [];
+
   constructor() { }
 
   ngOnInit() {
+    this.itemCount = this.goals.length;
   }
 
+  addItem(){
+    this.goals.push(this.goalText);
+    this.goalText = "";
+    this.itemCount += 1;
+  }
 }
