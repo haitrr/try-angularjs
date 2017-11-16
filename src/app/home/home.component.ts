42eaf48587690541d0f83d58fffcee125259c8d1
Hai Tran <hai.tran@2clicksolutions.com>
2017-11-16 11:28:48 +0700

    Animations and routings

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,14 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, style, transition, animate, keyframes, query, stagger } from '@angular/animations';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  animations: [
+    trigger('goals', [
+      transition('* => *', [
+        query(':enter', style({ opacity: 0 }), { optional: true }),
+        query(":enter", stagger('300ms', [
+          animate('.6s ease-in', keyframes([
+            style({ opacity: 0, transform: "translateY(-75%)", offset: 0 }),
+            style({ opacity: 0.5, transform: "translateY(35px)", offset: 0.5 }),
+            style({ opacity: 1, transform: "translateY(0)", offset: 1 }),
+          ]))
+        ]), { optional: true }),
+        query(":leave", stagger('300ms', [
+          animate('.6s ease-in', keyframes([
+            style({ opacity: 1, transform: "translateY(0)", offset: 0 }),
+            style({ opacity: 0.5, transform: "translateY(35px)", offset: 0.5 }),
+            style({ opacity: 0, transform: "translateY(-75%)", offset: 1 }),
+          ]))
+        ]), { optional: true }),
+      ])
+    ])
+  ]
 })
 export class HomeComponent implements OnInit {
 
-  itemCount : number = 4;
-  buttonText : string = "Add an item";
+  itemCount: number = 4;
+  buttonText: string = "Add an item";
   goalText = "My first life goal";
   goals = [];
 
@@ -18,9 +40,13 @@ export class HomeComponent implements OnInit {
     this.itemCount = this.goals.length;
   }
 
-  addItem(){
+  addItem() {
     this.goals.push(this.goalText);
     this.goalText = "";
     this.itemCount += 1;
+  }
+
+  removeItem(i:number){
+    this.goals.splice(i,1);
   }
 }
